e9c9e30b6ecc2a19a9ed7957e181d42902e27d75
Paul Chukwuemeka <paulchukwuemeka123@gmail.com>
2024-12-26 06:18:56 +0100

    feat: enhance header component with responsive menu and improve main layout styling

--- src/app/Components/header.tsx
+++ src/app/Components/header.tsx
@@ -1,38 +1,73 @@
-import React from "react";
+"use client";
+import React, {
+  useState,
+  useEffect,
+} from "react";
 import {
   FaRegUser,
   FaHome,
 } from "react-icons/fa";
+import { FaBars } from "react-icons/fa";
 import { AiOutlineFundProjectionScreen } from "react-icons/ai";
 import { IoDocumentText } from "react-icons/io5";
 
 const Header = () => {
+  const [menuState, setMenuState] =
+    useState(false);
   return (
     <div
-      className="sticky top-0 z-50 flex justify-around items-center bg-[#1a202c7a]
-    px-10 py-4 w-full text-white backdrop-blur-sm lg:py-10"
+      className="sticky bg-[#1a1a758f] top-0 z-50 flex flex-col justify-around items-center
+      px-10 py-4 h-fit text-white backdrop-blur-sm lg:py-4 "
     >
-      <h1 className="text-2xl font-extrabold text-blue-500">
-        PCE.
-      </h1>
-      <ul className=" nav flex gap-6 text-xl ">
-        <li>
-          <FaHome />
-          Home
-        </li>
-        <li>
-          <FaRegUser />
-          About
-        </li>
-        <li>
-          <AiOutlineFundProjectionScreen />
-          Projects
-        </li>
-        <li>
-          <IoDocumentText />
-          Resume
-        </li>
-      </ul>
+      <nav
+        className="flex justify-around items-center w-full
+       max-md:justify-between"
+      >
+        <h1 className="text-2xl lg:text-4xl font-extrabold text-blue-500">
+          PCE.
+        </h1>
+        <button className="text-3xl text-sky-500 lg:hidden">
+          <FaBars />
+        </button>
+        <ul className=" nav flex gap-6 text-xl max-md:hidden ">
+          <li>
+            <FaHome />
+            Home
+          </li>
+          <li>
+            <FaRegUser />
+            About
+          </li>
+          <li>
+            <AiOutlineFundProjectionScreen />
+            Projects
+          </li>
+          <li>
+            <IoDocumentText />
+            Resume
+          </li>
+        </ul>
+      </nav>
+      {menuState && (
+        <ul className=" nav flex-col text-xl">
+          <li>
+            <FaHome />
+            Home
+          </li>
+          <li>
+            <FaRegUser />
+            About
+          </li>
+          <li>
+            <AiOutlineFundProjectionScreen />
+            Projects
+          </li>
+          <li>
+            <IoDocumentText />
+            Resume
+          </li>
+        </ul>
+      )}
     </div>
   );
 };
--- src/app/Components/main.tsx
+++ src/app/Components/main.tsx
@@ -12,9 +12,9 @@ import Tilt from "react-parallax-tilt";
 
 const Main = () => {
   return (
-    <main className="text-white w-full flex flex-col items-center relative z-0 ">
+    <main className="text-white bg-[#5e5edf0e] w-full flex flex-col items-center relative -top-20 z-0 ">
       <section
-        className="font-bold text-4xl pt-24 flex justify-evenly flex-col 
+        className="font-bold text-4xl flex justify-evenly flex-col 
   md:flex-row  lg:items-center lg:justify-around lg:gap-8
 md:justify-around md:gap-1 gap-8 items-center bg-[#5e5edf21] w-full"
       >
@@ -24,13 +24,13 @@ md:justify-around md:gap-1 gap-8 items-center bg-[#5e5edf21] w-full"
             <span className="wave -mt-2">👋🏾</span>
           </h1>
           <h1>
-            I&apos;m
+            I{"'"}m{" "}
             <span className="text-blue-500">
               Paul Chukwuemeka
             </span>
           </h1>
           <h2 className="text-2xl font-bold">
-            I am a
+            I am a{" "}
             <span className="text-blue-500">
               <Typewriter
                 words={[
@@ -55,8 +55,8 @@ md:justify-around md:gap-1 gap-8 items-center bg-[#5e5edf21] w-full"
           className="lg:w-1/2"
         />
       </section>
-      <section className="flex flex-col items-center p-8 text-center">
-        <h2 className="text-4xl font-bold text-blue-500 mt-8">
+      <section className=" flex flex-col items-center p-8 text-center">
+        <h2 className="text-4xl font-bold text-blue-500 mt-8 ">
           About Me
         </h2>
         <p className="text-center text-xl lg:w-3/4 mt-4">
@@ -65,32 +65,31 @@ md:justify-around md:gap-1 gap-8 items-center bg-[#5e5edf21] w-full"
           user interfaces.
         </p>
         <p className="lg:w-3/4 mt-4 text-xl ">
-          I am also experienced in building
+          I am also experienced in building {""}
           <span className="text-blue-500 font-bold">
-            
             responsive web applications
-          </span>
+          </span>{" "}
           using modern web technologies.
         </p>
         <p className="lg:w-3/4 mt-4 text-xl ">
           Whenever possible, I also apply my
-          passion for developing products with
+          passion for developing products with{" "}
           <span className="text-blue-500 font-bold">
-            
             Node.js
-          </span>
-          and Modern
+          </span>{" "}
+          and Modern{" "}
           <span className="text-blue-500 font-bold text-xl ">
             JavaScript
-          </span>
-          Libraries and Frameworks like
+          </span>{" "}
+          Libraries and Frameworks like{" "}
           <span className="text-blue-500 font-bold">
             React
-          </span>
-          and
+          </span>{" "}
+          and{" "}
           <span className="text-blue-500 font-bold">
             Next.js
           </span>
+          .
         </p>
         <div className="text-blue-500">
           <Tilt
@@ -106,16 +105,35 @@ md:justify-around md:gap-1 gap-8 items-center bg-[#5e5edf21] w-full"
             <Image
               src={Profile}
               alt="profile"
-              className="rounded-full my-8 overflow-hidden w-[300px] h-[300px]"
+              className="rounded-2xl my-8 overflow-hidden 
+              w-[300px] h-[300px] shadow-md shadow-blue-300"
             />
           </Tilt>
           <h1 className="text-3xl font-bold ">
             Contact me on
           </h1>
-          <span className="flex items-center justify-center gap-10 text-3xl cursor-pointer ">
-            <FaGithub className="hover:text-blue-400" />
-            <FaLinkedin className="hover:text-blue-400" />
-            <FaTwitter className="hover:text-blue-400" />
+          <span
+            className="flex items-center justify-center gap-10 text-3xl 
+          font-bold cursor-pointer mt-4 "
+          >
+            <a
+              href="http://www.github.com/Paul-Chukwuemeka"
+              className="block hover:shadow-xl hover:shadow-blue-500 bg-white p-2 rounded-full"
+            >
+              <FaGithub />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/paulchukwuemeka"
+              className="block hover:shadow-xl hover:shadow-blue-500 bg-white p-2 rounded-full"
+            >
+              <FaLinkedin />
+            </a>
+            <a
+              href="https://x.com/PaulDProgrammer"
+              className="block hover:shadow-xl hover:shadow-blue-500 bg-white p-2 rounded-full"
+            >
+              <FaTwitter />
+            </a>
           </span>
         </div>
       </section>
